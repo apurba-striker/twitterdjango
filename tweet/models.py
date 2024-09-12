abbe1af5f03c4d2a7fcb6de543d7bfc1103d4edc
apurbapa <apurba@karmandrones.com>
2024-09-12 07:36:00 +0530

    first commit

---
+++ tweet/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Tweet(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField(max_length=240)
+    photo = models.ImageField(upload_to='photos/', blank=True, null = True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    
+    
+def __str__(self):
+    return f'{self.user.username} - {self.text[:10]}'
+# Create your models here.
